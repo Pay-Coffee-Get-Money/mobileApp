287fa2b9857f9d0fb1b713a66e12abdea78f1df4
quaqduy <duycnttdo@gmail.com>
2023-08-16 23:28:12 +0700

    modified subjectManagament.js

--- routes/subjectManagament.js
+++ routes/subjectManagament.js
@@ -7,7 +7,5 @@ route.get('/subject', subjectController.readSubject);
 route.get('/subject/:id', subjectController.getSubjectById);
 route.put('/subject/:id', subjectController.updateSubject);
 route.delete('/subject/:id', subjectController.deleteSubject);
-route.post('/subject/importExcel', upload.single("file"), subjectController.uploadExcel);
-route.get('/subject/exportExcel', subjectController.exportExcel);
 
 module.exports = route;
