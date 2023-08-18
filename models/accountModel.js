222181eb57bc973261eac710a1afde336f46e044
quaqduy <duycnttdo@gmail.com>
2023-08-18 15:04:41 +0700

    update restfull for forgot password

--- models/accountModel.js
+++ models/accountModel.js
@@ -11,9 +11,8 @@ const accountModel = {
         // Đăng nhập người dùng mới bằng email/password
         return auth.signInWithEmailAndPassword(data.email,data.password);
     },
-    signInWithGoogle(){
-        // const provider = new firebase.auth.GoogleAuthProvider();
-        // return firebase.auth().signInWithPopup(provider);
+    signOut(){
+        return auth.signOut();
     },
     resetPassword(email){
         return firebase.auth().sendPasswordResetEmail(email)
